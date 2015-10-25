360975eb71fd76f798a0506c8246b3f80e098ace
Ivan Medina <medinadiazivan@gmail.com>
2015-10-25 13:43:55 -0500

    Update query working good

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ var db = null;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -41,8 +42,6 @@ var app = {
             e.preventDefault();
             var document_type = $("#document-type").val();
             var document_number = $("#document-number").val();
-            console.log(document_type);
-            console.log(document_number);
             if (document_type == 0) {
                 $("#document-type").parent().addClass("has-error");
                 navigator.notification.alert("Debe seleccionar el tipo de documento", null, "Scolartic", "Entendido =)");
@@ -52,10 +51,13 @@ var app = {
                 navigator.notification.alert("Debe ingresar el número de documento", null, "Scolartic", "Entendido =)");
             }
             else {
-                console.log("jksasa");
-                var db = window.openDatabase("scolartic", "1.0", "Scolartic DB", 1000000);
-                console.log("jasjksa");
-                db.transaction(app.populateDB, app.errorDB, app.successDB);
+                db = window.openDatabase("scolartic", "1.0", "Scolartic DB", 1000000);
+                db.transaction(function(tx) {
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS scolartic (dni unique, asistencia_jueves, asistencia_viernes, entrega_merchandising, entrega_usb)');
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS scolartic_error (log)');
+                    tx.executeSql('SELECT * FROM scolartic where dni = ?', [document_number], app.querySuccess, app.errorDB);
+                    tx.executeSql('SELECT * FROM scolartic_error', [], app.queryLogSuccess, app.errorDB);
+                }, app.errorDB, app.successDB);
             }
         });
         $("#document-type").change(function(e) {
@@ -89,32 +91,49 @@ var app = {
                 }
             }
         });
-    },
-    populateDB: function(tx) {
-        tx.executeSql('CREATE TABLE IF NOT EXISTS scolartic (dni unique, asistencia_jueves, asistencia_viernes, entrega_merchandising, entrega_usb)');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS scolartic_error (log)');
-        tx.executeSql('SELECT * FROM scolartic', [], app.querySuccess, app.errorDB);
-        tx.executeSql('SELECT * FROM scolartic_error', [], app.queryLogSuccess, app.errorDB);
-        
+        $(".update-action").click(function(e) {
+            e.preventDefault();
+            var id = $(this).attr("id");
+            var span_id = "#" + id.replace("btn", "msg");
+            var field = $(this).attr("data-field");
+            var dni = $("#dni-searched").text();
+            db.transaction(function(tx) {
+                tx.executeSql('UPDATE scolartic set ' + field + ' =  ? where dni = ?', ["si", dni]);
+            }, app.errorDB, function() {
+                $("#" + id).addClass("hidden");
+                if ($(span_id).hasClass("hidden")) $(span_id).removeClass("hidden");
+            });
+            
+        });
     },
     errorDB: function(err) {
-        console.log(err);
         tx.executeSql('INSERT INTO scolartic_error (log) VALUES (' + err.message + ')');
     },
     successDB: function() {
         console.log("correctoooo!");
     },
     querySuccess: function(tx, results) {
-        console.log("query success!!");
         var existsUser = results.rows.length;
-        if (existsUser > 0) {
-            navigator.notification.alert("Usuario existe", null, "Scolartic", "Wiii");
-        } else {
-            var dni = $("#document-number").val();
-            console.log(dni);
+        var dni = $("#document-number").val();
+        var asistencia_jueves = "no";
+        var asistencia_viernes = "no";
+        var entrega_merchandising = "no";
+        var entrega_usb = "no";
+        if (existsUser == 0) {
             tx.executeSql('INSERT INTO scolartic (dni, asistencia_jueves, asistencia_viernes, entrega_merchandising, entrega_usb) VALUES ("' + dni + '", "no", "no", "no", "no")');
-            console.log("se insertoooo!!");
+        } else {
+            asistencia_jueves = results.rows.item(0).asistencia_jueves;
+            asistencia_viernes = results.rows.item(0).asistencia_viernes;
+            entrega_merchandising = results.rows.item(0).entrega_merchandising;
+            entrega_usb = results.rows.item(0).entrega_usb;
         }
+        $("#dni-searched").text(dni);
+        app.showAction(asistencia_jueves, "asist-jue");
+        app.showAction(asistencia_viernes, "asist-vie");
+        app.showAction(entrega_merchandising, "ent-mcd");
+        app.showAction(entrega_usb, "ent-usb");
+        var data_info = $("#data");
+        if (data_info.hasClass("hidden")) data_info.removeClass("hidden");
     },
     queryLogSuccess: function(tx, results) {
         var errorCount = results.rows.length;
@@ -122,5 +141,16 @@ var app = {
         for (var i = 0; i < errorCount; i++) {
             console.log("Error: " +results.rows.item(i).log);
         };
+    },
+    showAction: function(element, id) {
+        var btn_id = "#btn-" + id;
+        var span_id = "#msg-" + id;
+        if (element == "no") {
+            if ($(btn_id).hasClass("hidden")) $(btn_id).removeClass("hidden");
+            if (!$(span_id).hasClass("hidden")) $(span_id).addClass("hidden");
+        } else {
+            if (!$(btn_id).hasClass("hidden")) $(btn_id).addClass("hidden");
+            if ($(span_id).hasClass("hidden")) $(span_id).removeClass("hidden");
+        }
     }
 };
